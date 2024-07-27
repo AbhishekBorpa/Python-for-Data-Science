311802c773666159ea8787c5257f55a3c5e8f3ca
Abhishek <abhishekborpa@gmail.com>
2024-07-27 15:32:28 +0530

    String

--- Python Basics/06_Python_Basics.py
+++ Python Basics/06_Python_Basics.py
@@ -1,4 +1,7 @@
-# typecasting
+# typecasting : data-type conversion
 a = 1
 b = 2
 print(a+b)
+a1 = "Abhishek"
+b1 = "2345"
+print(int(a)+int(b1))
---
+++ Python Basics/07_Python_Basics.py
@@ -0,0 +1,7 @@
+# Input() : take input from user
+new_var = input("enter your name : ")
+print("my name is : ",new_var)
+
+New_num = int(input("enter First num : "))
+New_num_2 = int(input('enter Second num : ')) # if we don't use int() conversion then it will concatenate
+print(New_num+New_num_2)
