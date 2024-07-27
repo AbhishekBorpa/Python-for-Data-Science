968e10939ff6835508fbb8e4770219d2e2b69f84
Abhishek <abhishekborpa@gmail.com>
2024-07-27 13:07:11 +0530

    Final

--- Python Basics/03_Python_Basics.py
+++ Python Basics/03_Python_Basics.py
@@ -5,3 +5,10 @@ print("hey Programmer")
 # """
 # use """ """ for multiline comment
 # """
+print("helllo boys this is abhishek kuntal\n from borpa \"writting\" python in mac.")
+ # escape sequence is used to make some new functionality in program to neww function
+ # ex: \n, \" \", \t
+
+ # print()
+
+print("hey","Abhishek ",34,33 ,sep="@",end=" 0001\n")
