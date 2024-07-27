abdc8b6eb8825beff174bfc5bbb28dad4a33ce6e
Abhishek <abhishekborpa@gmail.com>
2024-07-27 12:51:50 +0530

    New Changes

--- Python Basics/02_Python_Basics.py
+++ Python Basics/02_Python_Basics.py
@@ -1,1 +1,5 @@
+# learn about python function 
 print("hey boys")
+print(12*12)
+print(20/5)
+# print() funtion gives us the output of the programs
---
+++ Python Basics/03_Python_Basics.py
@@ -0,0 +1,7 @@
+print("hey Programmer")
+
+# Comment
+# use # for single line comment
+# """
+# use """ """ for multiline comment
+# """
