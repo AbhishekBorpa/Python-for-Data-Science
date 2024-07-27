3d2cfc6dccd38c704a3073383fb39107eec849ef
Abhishek <abhishekborpa@gmail.com>
2024-07-27 14:43:05 +0530

    New File

---
+++ Python Basics/04_Python_Basics.py
@@ -0,0 +1,27 @@
+# variable
+a = 1000000000000000000
+# synatax var_name = var_value
+print(a)
+abhishek = "my name is abhishek"
+print(abhishek)
+c = None
+d = complex(2,4)
+
+#data typr
+'''
+1. int
+2. float
+3. complex, double,long
+4. boolean
+5. String
+6.
+Mutable: can be change
+Immutable: can't be change
+
+# everything in python is an object
+1. List
+2. Tuple
+3. Dictionary
+4. Set
+
+'''
