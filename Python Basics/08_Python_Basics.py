464e2fb1f795b7e574b0dec44891f575c98eadd6
Abhishek <abhishekborpa@gmail.com>
2024-07-27 15:45:01 +0530

    Ver

--- Python Basics/08_Python_Basics.py
+++ Python Basics/08_Python_Basics.py
@@ -1,1 +1,13 @@
 # String
+# name = "abhishek singh"
+# name_2 = '\n \"deepak\" Singh'
+# name_3 = name+name_2
+# print(name_3)
+
+name_4 = """
+hey Abhishek Singh
+this    is python program 
+ for learning more about string functions.
+
+""" # it can be single string ''' like this '''
+print(name_4[2:10])
