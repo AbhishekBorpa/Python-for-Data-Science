50a163fc4c9a45b58c3dbcef1c84d62562470770
Abhishek <abhishekborpa@gmail.com>
2024-07-27 17:34:09 +0530

    message

---
+++ Python Basics/09_Python_Basics.py
@@ -0,0 +1,18 @@
+# if-else conditional
+age = int(input("enter your age: "))
+print("man you age are " ,age, "year old" )
+# conditional operator : <,> ,==,<=,>=,!=
+if(age==19):
+  print("you can drive")
+#nested 
+elif(age>18):
+  print("apply your driving licence buddy")
+  if(age>21):
+    print("idiots")
+elif(age>=25):
+  print("you should not ask this uncle g")
+else:
+  print("you can't drive")
+
+# Nested if-else
+
