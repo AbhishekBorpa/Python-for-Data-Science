41735405204b62d6cf6495b1364eac55c6e4821b
Abhishek <abhishekborpa@gmail.com>
2024-07-27 12:09:43 +0530

    New Python Course

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python-for-Data-Science
 Learn Complete python for data science
+
+Python Download : https://www.python.com
+Replit: https://www.replit.com
---
+++ humour.py
@@ -0,0 +1,21 @@
+print("hey")
+# created  by guido von russo in 1991 and named after bbc TV show "The monty python show"
+# Use Replit to create  ease code platform
+# programming: use to tell computer to do something
+
+# python support functional and object-oriented language
+"""
+Feature:
+1. Ease and open source
+2. interpreted
+3. wide community
+4. lots of libraries
+
+
+Used for
+1. Data Science and Ai
+2. Developmment 
+"""
+
+print(5)
+print("hey how are you doing")
