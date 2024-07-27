54569a74e7a14720dc67fbbc5e1d1747e2c95ca5
Abhishek <abhishekborpa@gmail.com>
2024-07-27 15:02:07 +0530

    operator

---
+++ Python Basics/05_Python_Basics.py
@@ -0,0 +1,6 @@
+# operator in python
+# Arithmatic operator
+print(22*76)
+print(65+65) # addition
+print(30//10) # floar division
+print(45-40) # Subtract
---
+++ Python Basics/05_Python_Basics_Project.py
@@ -0,0 +1,10 @@
+ab = 10
+ba = 20
+ca = ab+ba
+da = ba-ab
+ea = ba*ab
+fa = ba/ab
+print(ca)
+print(da)
+print(ea)
+print(fa)
