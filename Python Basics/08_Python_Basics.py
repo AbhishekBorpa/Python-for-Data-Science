14c96d99a534ab1d1657693fe3952ecf2ceca484
Abhishek <abhishekborpa@gmail.com>
2024-07-27 17:14:03 +0530

    Changes

--- Python Basics/08_Python_Basics.py
+++ Python Basics/08_Python_Basics.py
@@ -1,13 +1,38 @@
 # String
-# name = "abhishek singh"
-# name_2 = '\n \"deepak\" Singh'
+name = "abhishek singh"
+name_2 = "deepak Singh"
 # name_3 = name+name_2
 # print(name_3)
 
-name_4 = """
-hey Abhishek Singh
-this    is python program 
- for learning more about string functions.
+name_4 = """ hey Abhishek Singh this is python program for learning more about string functions New""" 
+# it can be single string ''' like this '''
+#print(name_4[2:10])
 
-""" # it can be single string ''' like this '''
-print(name_4[2:10])
+# for i in name_4:
+#     print(i)
+#     print("New Line \n")
+
+# String Slicing
+
+# print(name_4)
+# print(len(name_4))
+# print(name_4[:14])
+# print(name_4[2:25:2])
+name_5 = "ABhishek Singh"
+# print(name_5[-10:-1])
+# print(name_5[0:4])
+print(name_5[-5:9])
+
+# string Methods
+print(name_5.upper())
+print(name_5.lower())
+print(name_5.rstrip("k"))
+print(name_5.replace("Singh","kuntal"))
+print(name_5.split("i"))
+
+print(name_2.capitalize())
+print(name_4.count("in"))
+
+print(name_4.endswith("New"))
+print(name_4.find("is")) # index()  for error message
+# isalpha(), isalnum(), islower(), isprintable(), istitle(), swapcase(lower -> upper case, vice-versa), title()
