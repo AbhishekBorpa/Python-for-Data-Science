711b1627f012979cc0ae01bbc0ed3685efbc2ba8
Abhishek <abhishekborpa@gmail.com>
2024-07-28 11:35:35 +0530

    set

---
+++ Python Core/03_Python_Core.py
@@ -0,0 +1,8 @@
+# sets : group of numbers : unordered collection of data 
+
+sets = {2,3,4,5,7,8}
+sets_2 = {2,3,4,"abhishek",True,34.52}
+print(sets)
+print(type(sets))
+sets_3 = set()
+print(type(sets_3))
