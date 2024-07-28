a69d1a9262ea748c94ea8c3b3e008c18ea4e2e1d
Abhishek <abhishekborpa@gmail.com>
2024-07-28 12:38:38 +0530

    us

--- Python Core/04_Python_Core.py
+++ Python Core/04_Python_Core.py
@@ -1,1 +1,36 @@
 # dictionary -> {key:value} pair
+dic = {
+  "namme":"abhishek",
+  "age":19,
+  "subject":"computer science"
+}
+print(dic.keys())
+print(dic.values())
+print(dic["age"])
+print(dic.get("age"))
+
+for valuess in dic.values():
+  print(valuess)
+
+print(dic.items())
+
+
+# methods  ordered way
+ep = {
+  1:10,
+  2:13,
+  3:9,
+  5:12,
+  4:14,
+  6:15,
+  7:8
+}
+ep2 = {
+  8:10,
+  9:12,
+  10:16,
+  11:13,
+  12:14
+}
+print(ep)
+ep.
