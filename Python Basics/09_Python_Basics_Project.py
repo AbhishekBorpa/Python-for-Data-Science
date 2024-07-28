fb31ee2a46b8ed9c82b40908df264b4bf039e160
Abhishek <abhishekborpa@gmail.com>
2024-07-28 10:41:21 +0530

    lil changes

--- Python Basics/09_Python_Basics_Project.py
+++ Python Basics/09_Python_Basics_Project.py
@@ -1,4 +1,18 @@
 import time
 
-time_1= time.strftime('%y')
+time_1= time.strftime('%H:%M:%S')
+hour = int(input("enter your hour :"))
+if(hour>=4 and hour<12):
+   print("good morning")
+
+elif (hour >=12 and hour <16):
+   print("good afternoon")
+
+elif(hour >=16 and hour< 19):
+
+   print("good evening")
+
+elif(hour>=19 and hour<24):
+   print("good night")
+
 print(time_1)
