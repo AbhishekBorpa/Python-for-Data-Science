aec5055a1c22d97d14c099dbe7da18691a7416e1
Abhishek <abhishekborpa@gmail.com>
2024-07-31 09:24:21 +0530

    gig

--- Python OOPS/04_Python_OOPS.py
+++ Python OOPS/04_Python_OOPS.py
@@ -1,24 +1,24 @@
-# inheritence 
-class employee:
-  def __init__(self,name,id):
-    self.id = id
-    self.name = name
+# # inheritence 
+# class employee:
+#   def __init__(self,name,id):
+#     self.id = id
+#     self.name = name
 
-  def ShowDetails(self):
-    print(f"id of Employes:{self.id} and name is {self.name}")
+#   def ShowDetails(self):
+#     print(f"id of Employes:{self.id} and name is {self.name}")
 
-employee1 = employee("abhishek",10)
-employee1.ShowDetails()
-
-
-class programmer(employee):
-  def ShowLanguage(self):
-    print("Default language is python in computer ")
+# employee1 = employee("abhishek",10)
+# employee1.ShowDetails()
 
 
-e2 = programmer("naveen singh",20)
-e2.ShowDetails()
-e2.ShowLanguage()
+# class programmer(employee):
+#   def ShowLanguage(self):
+#     print("Default language is python in computer ")
+
+
+# e2 = programmer("naveen singh",20)
+# e2.ShowDetails()
+# e2.ShowLanguage()
 
 
 # types 
@@ -31,4 +31,38 @@ e2.ShowLanguage()
 
 """
 
+#   single Inheritance
+
+class animal:
+  def __init__(self,name,speccies):
+    self.name = name 
+    self.species = speccies
+
+  def make_sound(self):
+    print("sound made by animal")
+
+
+class dog(animal):
+  def __init__(self,name,breed):
+    animal.__init__(self,name,speccies="Dog")
+    self.breed = breed
+  def make_sound(self):    # inheritence
+    print("Bark!")
+
+d = dog("max","dog")
+d.make_sound()
+
+a = animal("maximum","human")
+a.make_sound()
+
+
+#          Multiple inheritence -> single child multiple inheritence
+
+#          Multilevel inheritence -> inheretent from inhertence class ### main class -> inheritence -> second inheritence
+# employess -> programmer -> Data scientist
+
+# hybrid and hierarchical inheritence
+
+
+ 
 
